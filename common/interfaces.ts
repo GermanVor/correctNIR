b3d10cf6c62daf9fba3885d95420a22f6628665c
GermanVor <vorotnikov.german@yandex.ru>
2022-05-23 11:14:12 +0300

    remove commob dir

--- common/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export type CoordinateType = [number, number];
-
-export type AirportType = {
-    icao: string;
-    name: string;
-    coordinats: CoordinateType;
-};
--- common/requestName.ts
+++
@@ -1,3 +0,0 @@
-export enum RequestName {
-    GET_AIRPORTS = "GET_AIRPORTS",
-}
